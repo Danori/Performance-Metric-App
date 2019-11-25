ac5a010093a35ff8daed95635c576e1c195e44ef
Danori <lilbill1120@aol.com>
2019-11-25 14:04:51 -0500

    Update rendercharts.js

--- rendercharts.js
+++ rendercharts.js
@@ -1,11 +1,11 @@
-window.onload = () => { 
+window.onload = () => {
+    // Initialize all the datapoint arrays.
     let lowBeta_af3 = [];
     let highBeta_af3 = [];
     let alpha_af3 = [];
     let theta_af3 = [];
     let engagement_af3 = [];
     let fatigue_af3 = [];
-
     let lowBeta_af4 = [];
     let highBeta_af4 = [];
     let alpha_af4 = [];
@@ -13,6 +13,7 @@ window.onload = () => {
     let engagement_af4 = [];
     let fatigue_af4 = [];
 
+    // Define the AF3 Spline graph.
     let splineGraph_af3 = new CanvasJS.Chart("chartContainer_af3", {
         title: {
             text: "Performance Metrics - AF3"
@@ -67,6 +68,7 @@ window.onload = () => {
         }]
     });
 
+    // Define the AF4 spline graph.
     let splineGraph_af4 = new CanvasJS.Chart("chartContainer_af4", {
         title: {
             text : "Performance Metrics - AF4"
@@ -121,109 +123,184 @@ window.onload = () => {
         }]
     });
 
-    let time = -50;
-    let updateInterval = 1000;
-    let timeWindow = -time;
+    // Initialize the websocket to communicate with the BCI interface server.
+    const socketUrl = "ws://localhost:8080";
+    const ws = new WebSocket(socketUrl);
 
-    updateCharts = (count) => {
-        const socketUrl = "ws://localhost:8080";
-        const ws = new WebSocket(socketUrl);
+    // Initial time values.
+    let time = -10.0;
+    let updateInterval = 100;
+    let timeWindow = 100;
+    let data = JSON;
 
-        ws.onopen = (evt) => {
-            ws.send("Requesting brain data ... " + Date.now());
-        };
+    // Fill the data point arrays with initial values (just zeroes).
+    while (time < 0) {
+        theta_af3.push({
+            x: time,
+            y: 0
+        });
 
-        ws.onmessage = (evt) => {
-            let data = JSON.parse(evt.data);
+        alpha_af3.push({
+            x: time,
+            y: 0
+        });
 
-            count = count || 1;
-            for (let j = 0; j < count; j++) {	
-                theta_af3.push({
-                    x: time,
-                    y: data['pow'][0]
-                });
+        lowBeta_af3.push({
+            x: time,
+            y: 0
+        });
 
-                alpha_af3.push({
-                    x: time,
-                    y: data['pow'][1]
-                });
+        highBeta_af3.push({
+            x: time,
+            y: 0
+        });
 
-                lowBeta_af3.push({
-                    x: time,
-                    y: data['pow'][2]
-                });
+        engagement_af3.push({
+            x: time,
+            y: 0
+        });
 
-                highBeta_af3.push({
-                    x: time,
-                    y: data['pow'][3]
-                });
+        fatigue_af3.push({
+            x: time,
+            y: 0
+        });
 
-                engagement_af3.push({
-                    x: time,
-                    y: (data['pow'][3] / (data['pow'][1] + data['pow'][0]))
-                })
+        theta_af4.push({
+            x: time,
+            y: 0
+        });
 
-                fatigue_af3.push({
-                    x: time,
-                    y: ((data['pow'][1] + data['pow'][0]) / data['pow'][2])
-                })
+        alpha_af4.push({
+            x: time,
+            y: 0
+        });
 
-                theta_af4.push({
-                    x: time,
-                    y: data['pow'][20]
-                });
+        lowBeta_af4.push({
+            x: time,
+            y: 0
+        });
 
-                alpha_af4.push({
-                    x: time,
-                    y: data['pow'][21]
-                });
+        highBeta_af4.push({
+            x: time,
+            y: 0
+        });
 
-                lowBeta_af4.push({
-                    x: time,
-                    y: data['pow'][22]
-                });
+        engagement_af4.push({
+            x: time,
+            y: 0
+        });
 
-                highBeta_af4.push({
-                    x: time,
-                    y: data['pow'][23]
-                });
+        fatigue_af4.push({
+            x: time,
+            y: 0
+        });
 
-                engagement_af4.push({
-                    x: time,
-                    y: (data['pow'][23] / (data['pow'][21] + data['pow'][20]))
-                })
-
-                fatigue_af4.push({
-                    x: time,
-                    y: ((data['pow'][21] + data['pow'][20]) / data['pow'][22])
-                })
-
-                time++;
-            }
-
-            
-            if (lowBeta_af3.length > timeWindow) {
-                lowBeta_af3.shift();
-                highBeta_af3.shift();
-                alpha_af3.shift();
-                theta_af3.shift();
-                engagement_af3.shift();
-                fatigue_af3.shift();
-                lowBeta_af4.shift();
-                highBeta_af4.shift();
-                alpha_af4.shift();
-                theta_af4.shift();
-                engagement_af4.shift();
-                fatigue_af4.shift();
-            }
-
-            splineGraph_af3.render();
-            splineGraph_af4.render();
-        }
+        time += 0.1;
     }
 
-    updateCharts(timeWindow); 
+    // Communicate to the server when the web page is opened.
+    ws.onopen = (evt) => {
+        ws.send("Web application connected ... " + Date.now());
+    };
+
+    // Every time the server responds to brain data requests,
+    // Update the datapoints with the most recent data.
+    ws.onmessage = (evt) => {
+        data = JSON.parse(evt.data);
+        	
+        theta_af3.push({
+            x: time,
+            y: data['pow'][0]
+        });
+
+        alpha_af3.push({
+            x: time,
+            y: data['pow'][1]
+        });
+
+        lowBeta_af3.push({
+            x: time,
+            y: data['pow'][2]
+        });
+
+        highBeta_af3.push({
+            x: time,
+            y: data['pow'][3]
+        });
+
+        engagement_af3.push({
+            x: time,
+            y: (data['pow'][3] / (data['pow'][1] + data['pow'][0]))
+        });
+
+        fatigue_af3.push({
+            x: time,
+            y: ((data['pow'][1] + data['pow'][0]) / data['pow'][2])
+        });
+
+        theta_af4.push({
+            x: time,
+            y: data['pow'][20]
+        });
+
+        alpha_af4.push({
+            x: time,
+            y: data['pow'][21]
+        });
+
+        lowBeta_af4.push({
+            x: time,
+            y: data['pow'][22]
+        });
+
+        highBeta_af4.push({
+            x: time,
+            y: data['pow'][23]
+        });
+
+        engagement_af4.push({
+            x: time,
+            y: (data['pow'][23] / (data['pow'][21] + data['pow'][20]))
+        });
+
+        fatigue_af4.push({
+            x: time,
+            y: ((data['pow'][21] + data['pow'][20]) / data['pow'][22])
+        });
+
+        time += 0.1;
+        
+        // Shift the window if the graphs are full.
+        if (lowBeta_af3.length > timeWindow) {
+            lowBeta_af3.shift();
+            highBeta_af3.shift();
+            alpha_af3.shift();
+            theta_af3.shift();
+            engagement_af3.shift();
+            fatigue_af3.shift();
+
+            lowBeta_af4.shift();
+            highBeta_af4.shift();
+            alpha_af4.shift();
+            theta_af4.shift();
+            engagement_af4.shift();
+            fatigue_af4.shift();
+        }
+
+        // Update the graph renders with the new data.
+        splineGraph_af3.render();
+        splineGraph_af4.render();
+    }
+
+    // Request data from the server.
+    requestData = () => {
+        if (ws.readyState == ws.OPEN) {
+            ws.send("Requesting brain data ... " + Date.now());
+        }
+    }
+    
+    // Request data on an updateInterval.
     setInterval(() => { 
-        updateCharts();
+        requestData();
     }, updateInterval); 
 }
